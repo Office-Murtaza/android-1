337d5f36c528b3fba679c89daa0aa8b84662446a
Vikas Patel <vikas.patel15793@gmail.com>
2019-06-28 00:36:20 +0530

    Use sql file instead of xml in the liquibase

---
+++ backend/batm-api/src/main/resources/config/liquibase/changelog/create_user.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `w_user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(50) NOT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `created_date` timestamp NULL DEFAULT NULL,
+  `reset_date` timestamp NULL DEFAULT NULL,
+  `last_modified_by` varchar(50) DEFAULT NULL,
+  `last_modified_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `ux_user_phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
