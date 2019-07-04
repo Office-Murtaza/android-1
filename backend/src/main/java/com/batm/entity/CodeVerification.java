a2361530f4ff002330bd3a1bc323568556ec3339
Vikas Patel <vikas.patel15793@gmail.com>
2019-07-04 17:42:45 +0530

    Update user and verification code relation

--- backend/src/main/java/com/batm/entity/CodeVerification.java
+++ backend/src/main/java/com/batm/entity/CodeVerification.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -32,7 +32,7 @@ public class CodeVerification extends AbstractAuditingEntity implements Serializ
 	@Column(name = "code_id")
 	private Long codeId;
 
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
 
