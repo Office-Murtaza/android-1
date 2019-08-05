869dfbf2143dc8cd1fe6fad9bb2b380ad93e78c2
Alexander Mamayenko <mamayenko.alexander@gmail.com>
2019-08-06 02:23:40 +0300

    toolbar background fix

---
+++ android/app/src/main/java/com/app/belcobtm/ui/main/settings/phone/ShowPhoneActivity.kt
@@ -0,0 +1,13 @@
+package com.app.belcobtm.ui.main.settings.phone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.belcobtm.R
+
+class ShowPhoneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_phone)
+    }
+}
