4846af6bbab87fde9003e8468d4bd903812fbe2b
kostya <kostya@dappway.io>
2019-10-18 15:49:50 +0300

    Liquibase refactor

--- backend/src/main/java/com/batm/config/BeanConfiguration.java
+++ backend/src/main/java/com/batm/config/BeanConfiguration.java
@@ -5,17 +5,13 @@ import com.binance.api.client.BinanceApiRestClient;
 import com.binance.dex.api.client.BinanceDexApiClientFactory;
 import com.binance.dex.api.client.BinanceDexApiRestClient;
 import com.binance.dex.api.client.BinanceDexEnvironment;
-import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.client.RestTemplate;
-import javax.sql.DataSource;
 import java.util.Optional;
-import java.util.concurrent.Executor;
 
 @Configuration
 public class BeanConfiguration {
@@ -46,15 +42,5 @@ public class BeanConfiguration {
     @Bean
     public AuditorAware<String> getAuditorAware() {
         return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
-    }
-
-    @Bean
-    public SpringLiquibase liquibase(@Qualifier("taskExecutor") Executor executor, DataSource dataSource) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog("classpath:liquibase/changelog.xml");
-        liquibase.setShouldRun(true);
-
-        return liquibase;
     }
 }
