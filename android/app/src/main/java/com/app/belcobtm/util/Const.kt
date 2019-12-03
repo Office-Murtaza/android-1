9cdc7953bd5de3a0b49031930fa5bc63af8403de
ksatalsin <ksatalsin@gmail.com>
2019-12-03 19:40:55 +0200

    changed to prod URL

--- android/app/src/main/java/com/app/belcobtm/util/Const.kt
+++ android/app/src/main/java/com/app/belcobtm/util/Const.kt
@@ -3,7 +3,8 @@ package com.app.belcobtm.util
 object Const {
 
     //    private const val BASE_URL = "http://206.189.204.44:8080"
-    private const val BASE_URL = "https://test.belcobtm.com"
+//    private const val BASE_URL = "https://test.belcobtm.com"
+    private const val BASE_URL = "https://prod.belcobtm.com"
     private const val BASE_API_URL = "$BASE_URL/api"
     const val API_URL = "$BASE_API_URL/v1/"
 
