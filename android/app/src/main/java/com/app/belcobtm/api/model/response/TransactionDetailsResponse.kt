0c51d46aaab8f312a48c1010d5a0c2d09f7cca18
m.v.ishchenko <Maksym.Ishchenko@evolve-consultants.co.uk>
2020-02-06 21:40:58 +0200

    - fixed bugs

--- android/app/src/main/java/com/app/belcobtm/api/model/response/TransactionDetailsResponse.kt
+++ android/app/src/main/java/com/app/belcobtm/api/model/response/TransactionDetailsResponse.kt
@@ -5,17 +5,18 @@ import java.io.Serializable
 
 
 data class TransactionDetailsResponse(
-    @SerializedName("txId") val txId: String,
+    @SerializedName("txId") val txId: String?,
     @SerializedName("txDbId") val txDbId: Int?,
     @SerializedName("link") val link: String,
-    @SerializedName("type") val type: Int,
+    @SerializedName("type") val type: Int?,
     @SerializedName("status") val status: Int,
     @SerializedName("cashStatus") val cashStatus: Int,
-    @SerializedName("cryptoAmount") val amount: Double,
-    @SerializedName("cryptoFee") val fee: Double,
-    @SerializedName("date2") val date: String,
-    @SerializedName("fromAddress") val fromAddress: String,
-    @SerializedName("toAddress") val toAddress: String,
+    @SerializedName("cryptoAmount") val amount: Double?,
+    @SerializedName("fiatAmount") val fiatAmount: Double?,
+    @SerializedName("cryptoFee") val fee: Double?,
+    @SerializedName("date2") val date: String?,
+    @SerializedName("fromAddress") val fromAddress: String?,
+    @SerializedName("toAddress") val toAddress: String?,
     @SerializedName("imageId") val imageId: String?,
     @SerializedName("message") val message: String?,
     @SerializedName("phone") val phone: String?,
@@ -23,7 +24,7 @@ data class TransactionDetailsResponse(
 
 ) : Serializable {
 
-    fun getSttusType(): TransactionStatusType = TransactionStatusType.values()[status]
+    fun getStatusType(): TransactionStatusType = TransactionStatusType.values()[status]
 
     fun getCashStatusType(): TransactionCashStatusType = TransactionCashStatusType.values()[cashStatus]
 }
