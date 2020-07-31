23aed0c3e1ce789dd781e050513ae494a1ac47a4
Mike <llaerto@gmail.com>
2020-08-01 01:22:37 +0300

    BELCO-185: Android Settings refactoring and redesign
    - pin code flow
    
    //todo
    - KYC
    - stack polishing

--- android/app/src/main/java/com/app/belcobtm/presentation/features/pin/code/PinCodeFragment.kt
+++ android/app/src/main/java/com/app/belcobtm/presentation/features/pin/code/PinCodeFragment.kt
@@ -5,10 +5,9 @@ import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.View
+import androidx.lifecycle.observe
 import com.app.belcobtm.App
 import com.app.belcobtm.R
-import com.app.belcobtm.presentation.core.extensions.hide
-import com.app.belcobtm.presentation.core.extensions.show
 import com.app.belcobtm.presentation.core.extensions.toggle
 import com.app.belcobtm.presentation.core.ui.fragment.BaseFragment
 import com.app.belcobtm.presentation.features.HostActivity
@@ -17,14 +16,22 @@ import org.koin.android.viewmodel.ext.android.viewModel
 
 class PinCodeFragment : BaseFragment() {
     private val viewModel: PinCodeViewModel by viewModel()
-    private val pinMode: String by lazy { requireArguments().getString(TAG_PIN_MODE, KEY_PIN_MODE_ENTER) }
+    private val pinMode: String by lazy {
+        requireArguments().getString(
+            TAG_PIN_MODE,
+            KEY_PIN_MODE_ENTER
+        )
+    }
     override val resourceLayout: Int = R.layout.fragment_pin_code
     override val isToolbarEnabled: Boolean = false
     override val backPressedListener: View.OnClickListener = View.OnClickListener { /**empty**/ }
-    override val retryListener: View.OnClickListener = View.OnClickListener { viewModel.authorize() }
+    override val retryListener: View.OnClickListener =
+        View.OnClickListener { viewModel.authorize() }
+
+    private var appliedState: PinCodeState? = null
 
     override fun initViews() {
-        refreshTitle()
+        viewModel.setMode(pinMode)
     }
 
     override fun initListeners() {
@@ -40,103 +47,59 @@ class PinCodeFragment : BaseFragment() {
         key0View.setOnClickListener { addPinSymbol(it) }
         keyEraseView.setOnClickListener { removePinSymbol() }
         backButtonView.setOnClickListener {
-            pinIndicatorsView.setText("")
-            viewModel.enteredPinCode = ""
-            viewModel.enteredCreatePinCode = ""
-            errorView.hide()
-            refreshTitle()
-            refreshBackButton()
+            viewModel.onBackClick()
         }
     }
 
     override fun initObservers() {
-        viewModel.authorizationLiveData.listen({
-            (requireActivity() as HostActivity).showMainScreen()
-        })
+        viewModel.stateData.observe(this) { state ->
+            state.isLoading.doIfChanged(appliedState?.isLoading) {
+                if (it) {
+                    showLoading()
+                } else {
+                    showContent()
+                }
+            }
+            state.visiblePin.doIfChanged(appliedState?.visiblePin) {
+                pinIndicatorsView.setText(it)
+            }
+            state.labelResource.doIfChanged(appliedState?.labelResource) {
+                titleView.setText(it)
+            }
+            state.isError.doIfChanged(appliedState?.isError) {
+                errorView.toggle(it)
+                if (it) {
+                    vibrateError()
+                }
+            }
+            state.backButtonVisible.doIfChanged(appliedState?.backButtonVisible) {
+                backButtonView.toggle(it)
+            }
+        }
+        viewModel.actionData.observe(this) { action ->
+            when (action) {
+                is PinCodeAction.Success -> {
+                    (requireActivity() as HostActivity).showMainScreen()
+                }
+                is PinCodeAction.ChangedPin -> {
+                    navigate(R.id.pin_code_to_settings_fragment)
+                }
+                is PinCodeAction.Vibrate -> {
+                    vibrate(action.duration)
+                }
+                is PinCodeAction.AuthorizeError -> {
+                    showErrorServerError()
+                }
+            }
+        }
     }
 
     private fun addPinSymbol(view: View) {
-        val createdPin = viewModel.enteredCreatePinCode
-        val pin = viewModel.enteredPinCode
-        val selectedKey = view.tag as String
-        when {
-            pinMode != KEY_PIN_MODE_ENTER
-                    && pin.length == PIN_CODE_LENGTH
-                    && createdPin.length < PIN_CODE_LENGTH -> viewModel.enteredCreatePinCode += selectedKey
-            pin.length < PIN_CODE_LENGTH -> viewModel.enteredPinCode += selectedKey
-        }
-        onPinChange()
+        viewModel.onAddPinSymbol(view.tag as String)
     }
 
     private fun removePinSymbol() {
-        val createdPin = viewModel.enteredCreatePinCode
-        val pin = viewModel.enteredPinCode
-        when {
-            pinMode != KEY_PIN_MODE_ENTER
-                    && pin.length == PIN_CODE_LENGTH
-                    && createdPin.length <= PIN_CODE_LENGTH -> viewModel.enteredCreatePinCode = createdPin.dropLast(1)
-            pin.length < PIN_CODE_LENGTH -> viewModel.enteredPinCode = pin.dropLast(1)
-        }
-        onPinChange()
-    }
-
-    private fun onPinChange() {
-        val pinCode = when {
-            pinMode != KEY_PIN_MODE_ENTER && viewModel.enteredPinCode.length == PIN_CODE_LENGTH -> viewModel.enteredCreatePinCode
-            else -> viewModel.enteredPinCode
-        }
-        errorView.hide()
-        refreshBackButton()
-        refreshTitle()
-        pinIndicatorsView.setText(pinCode)
-        if (pinCode.length == PIN_CODE_LENGTH) {
-            onPinEntered(pinCode)
-        }
-    }
-
-    private fun onPinEntered(pinCode: String) {
-        when {
-            pinMode != KEY_PIN_MODE_ENTER && viewModel.enteredCreatePinCode.isBlank() -> {//first create/change pin screen
-                vibrate(100)
-                pinIndicatorsView.setText("")
-            }
-            pinMode != KEY_PIN_MODE_ENTER && viewModel.enteredCreatePinCode == viewModel.enteredPinCode -> {
-                vibrate(300)
-                viewModel.savePinCode(viewModel.enteredPinCode)
-                (requireActivity() as HostActivity).showMainScreen()
-            }
-            pinMode == KEY_PIN_MODE_ENTER && pinCode == viewModel.getSavedPinCode() -> {
-                vibrate(50)
-                viewModel.authorize()
-            }
-            pinMode == KEY_PIN_MODE_ENTER && pinCode != viewModel.getSavedPinCode() -> {
-                viewModel.enteredPinCode = ""
-                pinIndicatorsView.isError = true
-                errorView.show()
-                vibrateError()
-            }
-            else -> {//second create/change pin screen. pin doesn't phone
-                viewModel.enteredPinCode = ""
-                viewModel.enteredCreatePinCode = ""
-                pinIndicatorsView.isError = true
-                errorView.show()
-                vibrateError()
-            }
-        }
-    }
-
-    private fun refreshBackButton() =
-        backButtonView.toggle(pinMode != KEY_PIN_MODE_ENTER && viewModel.enteredPinCode.length == PIN_CODE_LENGTH)
-
-    private fun refreshTitle() {
-        when {
-            pinMode != KEY_PIN_MODE_ENTER && viewModel.enteredPinCode.length < PIN_CODE_LENGTH ->
-                titleView.setText(R.string.setup_pin_code)
-            pinMode != KEY_PIN_MODE_ENTER && viewModel.enteredPinCode.length == PIN_CODE_LENGTH ->
-                titleView.setText(R.string.pin_code_screen_confirm_pin)
-            pinMode == KEY_PIN_MODE_ENTER && viewModel.enteredPinCode.length < PIN_CODE_LENGTH ->
-                titleView.setText(R.string.pin_code_screen_enter_pin)
-        }
+        viewModel.onRemoveSymbol()
     }
 
     private fun vibrateError() {
@@ -171,6 +134,10 @@ class PinCodeFragment : BaseFragment() {
         const val KEY_PIN_MODE_CREATE = "key_pin_mode_create"
         const val KEY_PIN_MODE_CHANGE = "key_pin_mode_change"
         const val KEY_PIN_MODE_ENTER = "key_pin_mode_enter"
+
+        const val STEP_VERIFY = 0
+        const val STEP_CREATE = 1
+        const val STEP_CONFIRM = 2
 
         const val PIN_CODE_LENGTH: Int = 6
     }
--- android/app/src/main/java/com/app/belcobtm/presentation/features/pin/code/PinCodeViewModel.kt
+++ android/app/src/main/java/com/app/belcobtm/presentation/features/pin/code/PinCodeViewModel.kt
@@ -2,30 +2,190 @@ package com.app.belcobtm.presentation.features.pin.code
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.app.belcobtm.R
 import com.app.belcobtm.domain.authorization.interactor.AuthorizeUseCase
 import com.app.belcobtm.domain.authorization.interactor.GetAuthorizePinUseCase
 import com.app.belcobtm.domain.authorization.interactor.SaveAuthorizePinUseCase
+import com.app.belcobtm.presentation.core.SingleLiveData
 import com.app.belcobtm.presentation.core.mvvm.LoadingData
+import com.app.belcobtm.presentation.features.pin.code.PinCodeFragment.Companion.KEY_PIN_MODE_CHANGE
+import com.app.belcobtm.presentation.features.pin.code.PinCodeFragment.Companion.KEY_PIN_MODE_CREATE
+import com.app.belcobtm.presentation.features.pin.code.PinCodeFragment.Companion.KEY_PIN_MODE_ENTER
+import com.app.belcobtm.presentation.features.pin.code.PinCodeFragment.Companion.PIN_CODE_LENGTH
+import com.app.belcobtm.presentation.features.pin.code.PinCodeFragment.Companion.STEP_CONFIRM
+import com.app.belcobtm.presentation.features.pin.code.PinCodeFragment.Companion.STEP_CREATE
+import com.app.belcobtm.presentation.features.pin.code.PinCodeFragment.Companion.STEP_VERIFY
 
 class PinCodeViewModel(
     private val authorizeUseCase: AuthorizeUseCase,
     private val authorizePinUseCase: GetAuthorizePinUseCase,
     private val savePinCodeUseCase: SaveAuthorizePinUseCase
 ) : ViewModel() {
-    val authorizationLiveData = MutableLiveData<LoadingData<Unit>>()
-    var enteredCreatePinCode: String = ""
-    var enteredPinCode: String = ""
-
-    fun savePinCode(pinCode: String) = savePinCodeUseCase.invoke(SaveAuthorizePinUseCase.Params(pinCode))
+    fun savePinCode(pinCode: String) =
+        savePinCodeUseCase.invoke(SaveAuthorizePinUseCase.Params(pinCode))
 
     fun getSavedPinCode(): String = authorizePinUseCase.invoke()
 
     fun authorize() {
-        authorizationLiveData.value = LoadingData.Loading()
+        stateData.value = stateData.value!!.copy(isLoading = true)
         authorizeUseCase.invoke(
             Unit,
-            onSuccess = { authorizationLiveData.value = LoadingData.Success(it) },
-            onError = { authorizationLiveData.value = LoadingData.Error(it) }
+            onSuccess = { actionData.value = PinCodeAction.Success },
+            onError = {
+                enteredPin = ""
+                currentPin = ""
+                updateState()
+                actionData.value = PinCodeAction.AuthorizeError
+            }
         )
     }
+
+    val stateData = MutableLiveData<PinCodeState>(PinCodeState())
+    val actionData = SingleLiveData<PinCodeAction>()
+
+    private var enteredPin = ""
+
+    private var mode = KEY_PIN_MODE_ENTER
+    private var step = STEP_VERIFY
+    private var currentPin = ""
+    private var isError = false
+
+    fun setMode(mode: String) {
+        this.mode = mode
+        updateState()
+    }
+
+    fun onAddPinSymbol(symbol: String) {
+        isError = false
+        currentPin += symbol
+        if (currentPin.length > PIN_CODE_LENGTH) {
+            currentPin = ""
+            updateState()
+        }
+        if (currentPin.length == PIN_CODE_LENGTH) {
+            updateState()
+            onPinEntered()
+        } else {
+            updateState()
+        }
+    }
+
+    fun onRemoveSymbol() {
+        if (currentPin.isNotEmpty()) {
+            currentPin = currentPin.substring(0, currentPin.length - 1)
+            updateState()
+        }
+    }
+
+    fun onPinEntered() {
+        when {
+            step == STEP_CREATE -> saveFirstPinVersion()
+            step == STEP_CONFIRM -> validateConfirmedPin()
+            step == STEP_VERIFY && mode == KEY_PIN_MODE_ENTER -> validateUsualPinEnter()
+            step == STEP_VERIFY && mode == KEY_PIN_MODE_CHANGE -> validateOnChange()
+            else -> throw IllegalStateException("Wrong logic case, fix on dev side")
+        }
+    }
+
+    fun onBackClick() {
+        currentPin = ""
+        enteredPin = ""
+        isError = false
+        step = STEP_CREATE
+        updateState()
+    }
+
+    private fun validateOnChange() {
+        if (currentPin == getSavedPinCode()) {
+            verifiedOnChange()
+        } else {
+            currentPin = ""
+            wrongPin()
+        }
+    }
+
+    private fun validateUsualPinEnter() {
+        if (currentPin == getSavedPinCode()) {
+            vibrate(100)
+            authorize()
+        } else {
+            currentPin = ""
+            wrongPin()
+        }
+    }
+
+    private fun verifiedOnChange() {
+        currentPin = ""
+        step = STEP_CREATE
+        vibrate(100)
+        updateState()
+    }
+
+    private fun saveFirstPinVersion() {
+        enteredPin = currentPin
+        currentPin = ""
+        step = STEP_CONFIRM
+        vibrate(100)
+        updateState()
+    }
+
+    private fun validateConfirmedPin() {
+        if (enteredPin == currentPin) {
+            savePinCode(currentPin)
+            matchedPin()
+        } else {
+            step = STEP_CREATE
+            currentPin = ""
+            wrongPin()
+        }
+    }
+
+    private fun matchedPin() {
+        actionData.value = PinCodeAction.Success
+    }
+
+    private fun wrongPin() {
+        isError = true
+        updateState()
+    }
+
+    private fun vibrate(duration: Long) {
+        actionData.value = PinCodeAction.Vibrate(duration)
+    }
+
+    private fun updateState() {
+        stateData.value = stateData.value?.copy(
+            visiblePin = currentPin,
+            labelResource = getTitleRes(),
+            isError = isError,
+            backButtonVisible = mode != KEY_PIN_MODE_ENTER && currentPin.length == PIN_CODE_LENGTH
+        ) ?: PinCodeState()
+    }
+
+    private fun getTitleRes(): Int {
+        return when {
+            mode == KEY_PIN_MODE_ENTER -> R.string.pin_code_screen_enter_pin
+            mode == KEY_PIN_MODE_CREATE && step == STEP_CREATE -> R.string.setup_pin_code
+            mode == KEY_PIN_MODE_CREATE && step == STEP_CONFIRM -> R.string.pin_code_screen_confirm_pin
+            mode == KEY_PIN_MODE_CHANGE && step == STEP_VERIFY -> R.string.pin_code_screen_old_pin
+            mode == KEY_PIN_MODE_CHANGE && step == STEP_CREATE -> R.string.pin_code_screen_new_pin
+            mode == KEY_PIN_MODE_CHANGE && step == STEP_CONFIRM -> R.string.pin_code_screen_new_pin_confirm
+            else -> throw IllegalStateException("Wrong logic case, fix on dev side")
+        }
+    }
+}
+
+data class PinCodeState(
+    val visiblePin: String = "",
+    val labelResource: Int = R.string.pin_code_screen_enter_pin,
+    val isError: Boolean = false,
+    val backButtonVisible: Boolean = false,
+    val isLoading: Boolean = false
+)
+
+sealed class PinCodeAction {
+    data class Vibrate(val duration: Long) : PinCodeAction()
+    object Success : PinCodeAction()
+    object ChangedPin : PinCodeAction()
+    object AuthorizeError : PinCodeAction()
 }
