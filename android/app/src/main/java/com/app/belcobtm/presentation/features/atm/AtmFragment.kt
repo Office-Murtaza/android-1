30a3296efed1c9a3dec5f1ead0603d6209b91182
Mike <llaerto@gmail.com>
2020-08-26 20:56:57 +0300

    BELCO-258: Atm permission
    - fix permission set

--- android/app/src/main/java/com/app/belcobtm/presentation/features/atm/AtmFragment.kt
+++ android/app/src/main/java/com/app/belcobtm/presentation/features/atm/AtmFragment.kt
@@ -78,9 +78,8 @@ class AtmFragment : BaseFragment()
 
     @SuppressLint("MissingPermission")
     @NeedsPermission(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.CAMERA
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
     )
     fun onLocationPermissionGranted() {
         this.map?.isMyLocationEnabled = true
