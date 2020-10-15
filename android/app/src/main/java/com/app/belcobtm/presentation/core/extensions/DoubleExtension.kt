dd650b12dc404a6777f408f2a435e0d8b9467db1
Roman <roman_heshten@epam.com>
2020-10-15 12:02:21 +0300

    [BELCO-458] add scale field to CoinFee model, add truncate logic to double extension, exchange view model

--- android/app/src/main/java/com/app/belcobtm/presentation/core/extensions/DoubleExtension.kt
+++ android/app/src/main/java/com/app/belcobtm/presentation/core/extensions/DoubleExtension.kt
@@ -1,5 +1,6 @@
 package com.app.belcobtm.presentation.core.extensions
 
+import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
@@ -35,4 +36,12 @@ fun Double.toStringCoin(): String = if (this > 0) {
     }
 } else {
     "0"
+}
+
+fun Double.truncateDecimal(numberOfDecimals: Int): Double {
+    return if (this > 0) {
+        BigDecimal(this.toString()).setScale(numberOfDecimals, BigDecimal.ROUND_FLOOR)
+    } else {
+        BigDecimal(this.toString()).setScale(numberOfDecimals, BigDecimal.ROUND_CEILING)
+    }.toDouble()
 }
