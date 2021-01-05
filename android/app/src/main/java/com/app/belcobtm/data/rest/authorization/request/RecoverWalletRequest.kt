9d07f3d1c3dbc3b12e82097ffa9547681f2ec083
Roman <roman_heshten@epam.com>
2021-01-05 16:28:07 +0200

    add device info to the requests

--- android/app/src/main/java/com/app/belcobtm/data/rest/authorization/request/RecoverWalletRequest.kt
+++ android/app/src/main/java/com/app/belcobtm/data/rest/authorization/request/RecoverWalletRequest.kt
@@ -3,6 +3,9 @@ package com.app.belcobtm.data.rest.authorization.request
 data class RecoverWalletRequest(
     val phone: String,
     val password: String,
+    val deviceModel: String,
+    val deviceOS: String,
+    val appVersion: String,
     val notificationsToken: String?,
     val coins: List<RecoverWalletCoinRequest>,
     val platform: Int = 2//android
