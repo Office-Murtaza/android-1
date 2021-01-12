5428de29b08a67838138f8b1f3f187ae3d064b5b
Vadim_Kulinich <kulinichvadim29@gmail.com>
2021-01-12 17:10:11 +0200

    WALLET-600: add possibility to generate or past seed phrase

--- android/app/src/main/java/com/app/belcobtm/presentation/features/authorization/create/seed/CreateSeedViewModel.kt
+++ android/app/src/main/java/com/app/belcobtm/presentation/features/authorization/create/seed/CreateSeedViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import com.app.belcobtm.domain.authorization.interactor.CreateSeedUseCase
 import com.app.belcobtm.domain.authorization.interactor.CreateWalletUseCase
 import com.app.belcobtm.presentation.core.mvvm.LoadingData
-import com.app.belcobtm.presentation.features.authorization.create.seed.CreateSeedFragment.Companion.MODE_SETTINGS
 
 class CreateSeedViewModel(
     private val createPhraseUseCase: CreateSeedUseCase,
@@ -14,7 +13,7 @@ class CreateSeedViewModel(
     val seedLiveData: MutableLiveData<String> = MutableLiveData()
     val createWalletLiveData: MutableLiveData<LoadingData<Unit>> = MutableLiveData()
 
-    private fun createSeed() {
+    fun createSeed() {
         createPhraseUseCase.invoke(
             params = Unit,
             onSuccess = { seedLiveData.value = it }
@@ -28,11 +27,5 @@ class CreateSeedViewModel(
             onSuccess = { createWalletLiveData.value = LoadingData.Success(it) },
             onError = { createWalletLiveData.value = LoadingData.Error(it) }
         )
-    }
-
-    fun passArgs(args: CreateSeedFragmentArgs) {
-        if (args.mode != MODE_SETTINGS) {
-            createSeed()
-        }
     }
 }
