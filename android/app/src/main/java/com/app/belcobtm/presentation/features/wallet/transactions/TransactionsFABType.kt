219528635c6cb9bcd6c1e0a2978ae80ea5a45da2
Roman <roman_heshten@epam.com>
2021-01-14 23:04:21 +0200

    fix withdraw icon

--- android/app/src/main/java/com/app/belcobtm/presentation/features/wallet/transactions/TransactionsFABType.kt
+++ android/app/src/main/java/com/app/belcobtm/presentation/features/wallet/transactions/TransactionsFABType.kt
@@ -9,6 +9,6 @@ enum class TransactionsFABType(val id: Int, val resText: Int, val resIcon: Int) 
     SELL(1004, R.string.sell, R.drawable.ic_shopping_cart),
     RECALL(1006, R.string.recall, R.drawable.ic_recall),
     RESERVE(1007, R.string.reserve, R.drawable.ic_reserve),
-    WITHDRAW(1008, R.string.withdraw, R.drawable.ic_withdraw),
+    WITHDRAW(1008, R.string.withdraw, R.drawable.ic_vector_up),
     DEPOSIT(1009, R.string.deposit, R.drawable.ic_deposit)
 }
