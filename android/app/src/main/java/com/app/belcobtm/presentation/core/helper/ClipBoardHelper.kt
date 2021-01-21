f48c46b84ffa1ea9666bf9db1aec38e10fdc4033
Vadim_Kulinich <kulinichvadim29@gmail.com>
2021-01-21 18:40:39 +0200

    WALLET-627: fix wallet creation with seed phrase.

---
+++ android/app/src/main/java/com/app/belcobtm/presentation/core/helper/ClipBoardHelper.kt
@@ -0,0 +1,15 @@
+package com.app.belcobtm.presentation.core.helper
+
+import android.content.ClipboardManager
+import android.content.Context
+
+class ClipBoardHelper(context: Context) {
+
+    private val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+    fun getTextFromClipboard(): String? {
+        val clipData = clipboard.primaryClip
+        val item = clipData?.getItemAt(0)
+        return item?.text?.toString()
+    }
+}
