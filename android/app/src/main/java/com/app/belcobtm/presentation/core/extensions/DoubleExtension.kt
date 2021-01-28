6f470863dd7b12cd2b1365f3ec39d48901b51fd4
Roman <roman_heshten@epam.com>
2021-01-28 10:49:10 +0200

    [WALLET-608] add formatting

--- android/app/src/main/java/com/app/belcobtm/presentation/core/extensions/DoubleExtension.kt
+++ android/app/src/main/java/com/app/belcobtm/presentation/core/extensions/DoubleExtension.kt
@@ -4,28 +4,28 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
+fun Double.toStringPercents(): String = if (this > 0) {
+    formatWith(DecimalFormat("#.##"))
+} else {
+    "0"
+}
+
 fun Double.toStringUsd(): String = if (this > 0) {
-    val df = DecimalFormat("#.###")
-    df.roundingMode = RoundingMode.FLOOR
-    val trimmed = df.format(this)
-    if (trimmed.isNotBlank()) {
-        when (trimmed.last()) {
-            ',' -> trimmed.trimEnd(',')
-            '.' -> trimmed.trimEnd('.')
-            else -> trimmed.replaceFirst(',', '.')
-        }
-    } else {
-        "0"
-    }
+    formatWith(DecimalFormat("#.###"))
 } else {
     "0"
 }
 
 fun Double.toStringCoin(): String = if (this > 0) {
-    val df = DecimalFormat("#.######")
+    formatWith(DecimalFormat("#.######"))
+} else {
+    "0"
+}
+
+private fun Double.formatWith(df: DecimalFormat): String {
     df.roundingMode = RoundingMode.FLOOR
     val trimmed = df.format(this)
-    if (trimmed.isNotBlank()) {
+    return if (trimmed.isNotBlank()) {
         when (trimmed.last()) {
             ',' -> trimmed.trimEnd(',')
             '.' -> trimmed.trimEnd('.')
@@ -34,8 +34,6 @@ fun Double.toStringCoin(): String = if (this > 0) {
     } else {
         "0"
     }
-} else {
-    "0"
 }
 
 fun Double.withScale(scale: Int): Double {
